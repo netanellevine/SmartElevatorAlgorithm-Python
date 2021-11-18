0731221f437fea529fdccda43455363afa9bc92d
Netanel Levine <74298433+netanellevine@users.noreply.github.com>
2021-11-18 17:21:40 +0200

    Update Ex1_main.py

--- Ex1_main.py
+++ Ex1_main.py
@@ -5,14 +5,16 @@ import numpy as np
 def main():
     buildings = ["B1.json", "B2.json", "B3.json", "B4.json", "B5.json"]
     calls_cases = ["calls_a.csv", "calls_b.csv", "calls_c.csv", "calls_d.csv"]
-    Json.read_calculate_write(buildings[1], calls_cases[0], "1a")
-    Json.read_calculate_write(buildings[0], calls_cases[0], "2a")
+    buildings = ["B1.json", "B2.json", "B3.json", "B4.json", "B5.json"]
+    calls_cases = ["calls_a.csv", "calls_b.csv", "calls_c.csv", "calls_d.csv"]
+    Json.read_calculate_write(buildings[1], calls_cases[0], "_B1_a")
+    Json.read_calculate_write(buildings[0], calls_cases[0], "_B2_a")
     ind = 3
     for i in range(2, 5):
         ch = chr(97)
         ind1 = 1
         for j in calls_cases:
-            st = f'{ind}{ch}'
+            st = "_B" + str(ind) + "_" + ch
             Json.read_calculate_write(buildings[i], j, st)
             ch = chr(97 + ind1)
             ind1 += 1
